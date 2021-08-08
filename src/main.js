7f50fd67d9d81facd8837282b85daeb0b5c39218
Nelo Puchades Bresó <correo@nelo.dev>
2021-08-08 19:32:37 +0200

    Base commit - Refactored intro to vue 2 with VueX

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import BaseIcon from '@/components/BaseIcon.vue';
+
+Vue.component('BaseIcon', BaseIcon);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
